0985a263b23dc697b9e80cfc9309edcb422bdddb
ahnhk <basic@kangwon.ac.kr>
2020-11-26 05:45:29 +0900

    add comments for add to ReadMe

--- src/test/java/sig/oss/CalcTest.java
+++ src/test/java/sig/oss/CalcTest.java
@@ -7,6 +7,10 @@ public class CalcTest {
 		Calc c = new Calc();
 		assertEquals(30, c.add(10, 20));
 	}
-
+	@Test
+	public testSub() {
+		Cale c = new Cale();
+		assertEquals(-10, c. sub(10, 20));
+	}
 }
 
