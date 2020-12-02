53f9aa3a61d7bcaf8b46811df20a36db73fde56f
pyahn <45131505+pyahn@users.noreply.github.com>
2020-12-02 20:15:00 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,12 +1,13 @@
 public class Calc {
+	public int r=0;
 	public int add(int x, int y) { //2차커밋을 위한 내용변경
 		return x+y;
 	}
 	public int sub(int x, int y) {
 		return x-y;
 	}
-	public int inc(int d){ //미완성
-		return n;
+	public int dec(int d){ //미완성
+		r -= d;
 	}
 }
 
